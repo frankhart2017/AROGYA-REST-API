ff6c3172d50a4c6c633b81e83212917458586234
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2018-10-06 14:08:36 +0530

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb://admin:password98@@ds123753.mlab.com:23753/arogaya',
+  db_dev: 'mongodb://localhost:27017/login',
+};
