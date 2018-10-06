8784aada5ae058f595184a17129be346034c3bca
Rushil Gupta <37224850+rushil-care@users.noreply.github.com>
2018-10-06 18:51:31 +0530

    Update bmi.js

--- server/models/bmi.js
+++ server/models/bmi.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
+const bmiSchema = new mongoose.Schema({
 
+  uid: {
+    type: String,
+    default: ''
+  },
+  
+  bmi: {
+    type: String,
+    default: ''
+  }
+
+
+});
+
+module.exports = mongoose.model('BMI', bmiSchema);
