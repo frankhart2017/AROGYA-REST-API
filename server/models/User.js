4b37b1dfc53e604a58765517676f2d78c27c8968
Siddhartha Dhar Choudhury <38374913+frankhart2018@users.noreply.github.com>
2018-10-07 22:42:27 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -21,6 +21,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     default: ''
   },
+  accountType: {
+    type: String,
+    default: ''
+  },
 
   isDeleted: {
       type: Boolean,
