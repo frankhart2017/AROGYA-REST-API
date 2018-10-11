68c77fc0bef35effa79461da7e1b88312c956348
Rushil Gupta <37224850+rushil-care@users.noreply.github.com>
2018-10-11 16:07:51 +0530

    Update updateUser.js

--- server/routes/api/updateUser.js
+++ server/routes/api/updateUser.js
@@ -15,16 +15,103 @@ module.exports = (app) => {
 
         var _id = uid;
 
-        User.findOneAndUpdate({ _id: _id }, { $set: { Name: Name, password: bcrypt.hashSync(password, bcrypt.genSaltSync(8), null), gender: gender } }, { new: true }, function (err, doc) {
-            if (err) {
+       
 
-            }
-            return res.send({
-                success: true,
-                message: "FUCK OFF"
-            })
+        if (Name.length == 0 && gender.length == 0) {
+            
+            User.findOneAndUpdate({ _id: _id }, { $set: { password: bcrypt.hashSync(password, bcrypt.genSaltSync(8), null) } }, { new: true }, function (err, doc) {
+                if (err) {
 
-        });
+                }
+                return res.send({
+                    success: true,
+                    message: "Saved Successfully"
+                })
+
+            });
+        }
+
+        else if (Name.length == 0 && password.length == 0) {
+            User.findOneAndUpdate({ _id: _id }, { $set: { gender: gender } }, { new: true }, function (err, doc) {
+                if (err) {
+
+                }
+                return res.send({
+                    success: true,
+                    message: "Saved Successfully"
+                })
+
+            });
+        }
+
+        else if (gender.length == 0 && password.length == 0) {
+            User.findOneAndUpdate({ _id: _id }, { $set: { Name: Name } }, { new: true }, function (err, doc) {
+                if (err) {
+
+                }
+                return res.send({
+                    success: true,
+                    message: "Saved Successfully"
+                })
+
+            });
+        }
+
+        else if (Name.length == 0 && password.length != 0 && gender.length != 0) {
+            User.findOneAndUpdate({ _id: _id }, { $set: { gender: gender, password: bcrypt.hashSync(password, bcrypt.genSaltSync(8), null) } }, { new: true }, function (err, doc) {
+                if (err) {
+
+                }
+                return res.send({
+                    success: true,
+                    message: "Saved Successfully"
+                })
+
+            });
+        }
+
+        else if (Name.length != 0 && password.length == 0 && gender.length != 0) {
+            User.findOneAndUpdate({ _id: _id }, { $set: { Name: Name, gender: gender } }, { new: true }, function (err, doc) {
+                if (err) {
+
+                }
+                return res.send({
+                    success: true,
+                    message: "Saved Successfully"
+                })
+
+            });
+        }
+
+        else if (Name.length != 0 && password.length != 0 && gender.length == 0) {
+            User.findOneAndUpdate({ _id: _id }, { $set: { Name: Name, password: bcrypt.hashSync(password, bcrypt.genSaltSync(8), null) } }, { new: true }, function (err, doc) {
+                if (err) {
+
+                }
+                return res.send({
+                    success: true,
+                    message: "Saved SuccessfullyF"
+                })
+
+            });
+        }
+
+        else {
+            User.findOneAndUpdate({ _id: _id }, { $set: { Name: Name, password: bcrypt.hashSync(password, bcrypt.genSaltSync(8), null), gender: gender } }, { new: true }, function (err, doc) {
+                if (err) {
+
+                }
+                return res.send({
+                    success: true,
+                    message: "Saved Successfully"
+                })
+
+            });
+
+        }
+
+
+
 
 
 
