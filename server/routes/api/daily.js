d22b31662b44b3b1d11d6524c7f91ab97c1a69c8
Rushil Gupta <37224850+rushil-care@users.noreply.github.com>
2018-10-13 10:33:52 +0530

    Update daily.js

--- server/routes/api/daily.js
+++ server/routes/api/daily.js
@@ -74,4 +74,33 @@ module.exports = (app) => {
         });
     
     });
+
+
+
+
+    app.post('/api/account/fetchdaily', (req, res, next) => {
+
+        const { body } = req;
+    
+        const {
+            uid,
+        } = body;
+    
+        
+    
+        Daily.find({ uid: uid }, function (err, user) {
+            if (err) {
+    
+            }
+            console.log(user[0]);
+            return res.send(user[0]
+    
+            )
+    
+        });
+    
+    });
+
+
+
 }
